99e15e7ad2fae318bc562979229ce10044ee8e83
xiaoguangxian <143917434+xiaoguangxian@users.noreply.github.com>
2023-09-03 14:23:09 +0800

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	    scanf("%d",&a);
+	if (a%2==1)
+		printf("a是奇数\n");
+	else
+		printf("a是偶数\n"); 
+	return 0;
+}
